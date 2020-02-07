40f5ffc218948e8fe725bef884f5e5fa1a5e248f
benign-fork-5056 <gareeeliu@gmail.com>
2020-02-07 06:34:14 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -7,17 +7,17 @@ end
 WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
 def won?(board)
-  WIN_COMBINATIONS.each |wincombo|
-    win_index_1 = wincombo[0]
-    win_index_2 = wincombo[1]
-    win_index_3 = wincombo[2]
+  WIN_COMBINATIONS.each |wincomb|
+    win_index_1 = wincomb[0]
+    win_index_2 = wincomb[1]
+    win_index_3 = wincomb[2]
 
     position_1 = board[win_index_1]
     position_2 = board[win_index_2]
     position_3 = board[win_index_3]
 
   if position_1 == "X" && position_2 == "X" && positon_3 == "X"
-    return wincombo
+    return wincomb
   end
  return false
 end
