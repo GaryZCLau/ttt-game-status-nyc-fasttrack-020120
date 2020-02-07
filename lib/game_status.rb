b71ec4376787f34d2e6f61e60794a53a2996adc5
benign-fork-5056 <gareeeliu@gmail.com>
2020-02-07 08:37:43 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,28 +4,57 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
-WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+# WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+#
+# def won?(board)
+#   WIN_COMBINATIONS.each {|wincomb|
+#     win_index_1 = wincomb[0]
+#     win_index_2 = wincomb[1]
+#     win_index_3 = wincomb[2]
+#
+#     position_1 = board[win_index_1]
+#     position_2 = board[win_index_2]
+#     position_3 = board[win_index_3]
+#
+#   # if ((position_1 == "X") || (position_1 == "O") || (position_2 == "X") || (position_2 == "O") || (position_3 == "X") || (position_3 == "O"))
+#   #   return wincomb
+#   # end }
+#   # return false
+#   if position_1 == "X" || position_2 == "X" || position_3 == "X"
+#     return wincomb
+#   elsif position_1 == "O" || position_2 == "O" || position_3 == "O"
+#     return wincomb
+#   else
+#     return false
+#   end
+# }
+# end
+WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2]
+]
 
 def won?(board)
-  WIN_COMBINATIONS.each {|wincomb|
-    win_index_1 = wincomb[0]
-    win_index_2 = wincomb[1]
-    win_index_3 = wincomb[2]
+  WIN_COMBINATIONS.each {|win_combo|
+    index_0 = win_combo[0]
+    index_1 = win_combo[1]
+    index_2 = win_combo[2]
 
-    position_1 = board[win_index_1]
-    position_2 = board[win_index_2]
-    position_3 = board[win_index_3]
+    position_1 = board[index_0]
+    position_2 = board[index_1]
+    position_3 = board[index_2]
 
-  # if ((position_1 == "X") || (position_1 == "O") || (position_2 == "X") || (position_2 == "O") || (position_3 == "X") || (position_3 == "O"))
-  #   return wincomb
-  # end }
-  # return false
-  if position_1 == "X" || position_2 == "X" || position_3 == "X"
-    return wincomb
-  elsif position_1 == "O" || position_2 == "O" || position_3 == "O"
-    return wincomb
-  else
-    return false
-  end
-}
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combo
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return win_combo
+    end
+  }
+  return false
 end
